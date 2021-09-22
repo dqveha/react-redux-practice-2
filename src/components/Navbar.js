9adf7cd2c8f685863387745deeb9e72d87b90d0c
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-09-22 08:21:21 -0700

    Change <a> to <Link> to reflect React UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
       <nav className="nav-wrapper red darken-3">
         <div className="container">
-          <a href="/" className="brand-logo left">Some'Times</a>
+          <Link to="/" className="brand-logo left">Some'Times</Link>
           <ul className="right">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
         </div>
       </nav>
