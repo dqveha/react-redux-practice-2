49f6b7033b904327d65edb9f80132ffd7b43f652
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-09-22 08:02:25 -0700

    Add Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <div className="container">
+        <h4 className="center">Contact</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam id ad nam fuga nesciunt ab similique eaque temporibus, aut dolorum ratione earum porro, ut aperiam, molestias autem doloremque perspiciatis sequi.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
