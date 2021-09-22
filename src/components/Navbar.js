a4e2bafd44ba40e4ae03252d1a109795a6eae779
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-09-22 09:04:38 -0700

    Add HOC withRouter to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, withRouter } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = (props) => {
+  setTimeout(() => {
+    props.history.push('/about')
+  }, 2000);
   return (
     <div>
       <nav className="nav-wrapper red darken-3">
@@ -18,4 +21,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default withRouter(Navbar) //SUPERCHARGING THE COMPONENT!
